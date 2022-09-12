bb8c1cf3368fea1fe65c4faeba5bd90f0b17059e
MrKep <99817924+MrKep@users.noreply.github.com>
2022-09-12 19:17:02 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -45,8 +45,6 @@ class Training:
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         pass
-        # При использовании raise NotImplementedError()
-        # не получется пройти автопроверку
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
